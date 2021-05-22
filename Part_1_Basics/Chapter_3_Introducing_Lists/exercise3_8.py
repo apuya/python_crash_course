7e14e30088fadfc72d873fb2685c13f5f0e6c966
lostsaints <contact@mapuya.com>
2021-05-21 21:43:50 -0700

    solve exercise problems

--- Part_1_Basics/Chapter_3_Introducing_Lists/exercise3_8.py
+++ Part_1_Basics/Chapter_3_Introducing_Lists/exercise3_8.py
@@ -1,7 +1,7 @@
 # Python Crash Course: A Hands-On, Project-Based Introduction To Programming
 #
 # Name: Mark Lester Apuya
-# Date:
+# Date: 05/21/2021
 #
 # Chapter 3: Introducing Lists
 #
@@ -10,7 +10,7 @@
 #
 # • Store the locations in a list. Make sure the list is not in alphabetical order.
 # • Print your list in its original order. Don’t worry about printing the list neatly, just print it 
-#   as a raw Python list.# 
+#   as a raw Python list.
 # • Use sorted() to print your list in alphabetical order without modifying the actual list.
 # • Show that your list is still in its original order by printing it.
 # • Use sorted() to print your list in reverse alphabetical order without chang- ing the order of the original list.
@@ -21,3 +21,30 @@
 #   has been changed.
 # • Use sort() to change your list so it’s stored in reverse alphabetical order. Print the list to show that 
 #   its order has changed.
+
+# List of locations
+locations = ['Bali', 'Japan', 'Philippines', 'Iceland']
+print(locations)
+
+# sorted(): allows you display the list in alphabetical order without modifying the original list
+print(sorted(locations))
+print(locations)
+
+# sorted() function to display list in reverse alphabetical order
+# reverse = True will sort the list in reverse
+print(sorted(locations, reverse = True))
+print(locations)
+
+# reverse() will reverse the order of the list
+locations.reverse()
+print(locations)
+
+locations.reverse()
+print(locations)
+
+# sort() will change the order of the list alphabeticaly and reverse alphabettical
+locations.sort()
+print(locations)
+
+locations.sort(reverse = True)
+print(locations)
