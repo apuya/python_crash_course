376325ebe444ca0a13475cdbfcfa53766b600cec
lostsaints <contact@mapuya.com>
2021-05-28 00:02:16 -0700

    solve exercise problem

--- Part_1_Basics/Chapter_5_if_Statments/hello_admin.py
+++ Part_1_Basics/Chapter_5_if_Statments/hello_admin.py
@@ -1,12 +1,12 @@
 # Python Crash Course: A Hands-On, Project-Based Introduction To Programming
 #
 # Name: Mark Lester Apuya
-# Date:
+# Date: 05/27/2021
 #
 # Chapter 5: If Statements
 #
 # Exercise 5.8 Hello Admin:
-# Make a list of five or more usernames, including the name 'admin'. Imagine 
+#   Make a list of five or more usernames, including the name 'admin'. Imagine  
 # you are writing code that will print a greeting to each user after they log 
 # in to a website. Loop through the list, and print a greeting to each user:
 #
@@ -14,3 +14,11 @@
 #   would you like to see a status report?
 # • Otherwise, print a generic greeting, such as Hello Jaden, thank you for 
 #   logging in again.
+
+usernames = ['admin', 'mark', 'jaxon', 'troy', 'alex']
+
+for user in usernames:
+    if user == 'admin':
+        print("Hello admin, would you like to see the status report?")
+    else:
+        print("Hello " + user.title() + ", you are currently logged in")
